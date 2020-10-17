ead54a42e9814f92e2588a5c49a4a73b8a672d26
satskrishnan <47655494+satskrishnan@users.noreply.github.com>
2020-10-17 16:38:14 +0530

    Updated profile Updated profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,28 +6,25 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Baran Cezayirli | Technologist`,
-    author: `Baran Cezayirli`,
-    description: `With 10+ years of experience in various industries, I help tech
-              startups from idea phase to high volumes. I design and build
-              scalable solutions, implement internal processes that fit your
-              organization, build teams that produce results.`,
-    siteUrl: `https://barancezayirli.com/`,
+    title: `SathishKumar Krishnan | Principal Consultant`,
+    author: `Sathishkumar Krishnan`,
+    description: `I'm a Technical Consultant with more than 16+ Years of Experience in Banking Product Development, Consulting, Implementation, and Support.`,
+    siteUrl: `https://sathishkrishnan.com/`,
     social: {
-      twitter: `barancezayirli`,
+      twitter: `sathishkr`,
     },
   },
   plugins: [
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Baran Cezayirli | Technologist`,
+        name: `Sathishkumar Krishnan | Principal Consultant`,
         short_name: `CV`,
         start_url: `/`,
         background_color: `#f7f0eb`,
         theme_color: `#38B2AC`,
         display: `standalone`,
-        icon: 'src/assets/site-icon.png'
+        icon: 'src/assets/profile.png',
       },
     },
     'gatsby-plugin-offline',
@@ -54,7 +51,7 @@ module.exports = {
       options: {
         printRejected: true,
         tailwind: true, // Enable tailwindcss support
-      }
-    }
+      },
+    },
   ],
 };
